779b6303a7f380bdb5449529b7064606ed782b55
James Lundin <jlundin@raptive.com>
2023-12-10 15:01:59 -0500

    complete day 1 part 1

--- day-01/src/bin/part_1.rs
+++ day-01/src/bin/part_1.rs
@@ -4,8 +4,24 @@ fn main() {
     dbg!(output);
 }
 
-fn part1(input: &str) -> String {
-    "todo!()".to_string()
+fn part1(input: &str) -> u32 {
+    let mut total = 0;
+    for line in input.lines() {
+        let mut first_number = String::new();
+        let mut last_number = String::new();
+        for c in line.chars() {
+            if c.is_digit(10) {
+                if first_number.is_empty() {
+                    first_number = c.to_string();
+                }
+                last_number = c.to_string();
+            }
+        }
+        let combined_numbers = format!("{first_number}{last_number}");
+        let combined_numbers: u32 = combined_numbers.parse().unwrap();
+        total += combined_numbers;
+    }
+    total
 }
 
 #[cfg(test)]
@@ -14,7 +30,12 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = part1("");
-        assert_eq!(result, "4".to_string());
+        let result: u32 = part1(
+            "1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet",
+        );
+        assert_eq!(result, 142);
     }
 }
--- day-01/src/bin/part_2.rs
+++ day-01/src/bin/part_2.rs
@@ -1,11 +1,31 @@
 fn main() {
-    let input = include_str!("input_2.txt");
+    let input = include_str!("input_1.txt");
     let output = part1(input);
     dbg!(output);
 }
 
-fn part1(input: &str) -> String {
-    "todo!()".to_string()
+fn part1(input: &str) -> u32 {
+    let mut total = 0;
+    for line in input.lines() {
+        let mut first_number = String::new();
+        let mut last_number = String::new();
+        for c in line.chars() {
+            if c.is_digit(10) {
+                if first_number.is_empty() {
+                    first_number = c.to_string();
+                }
+                last_number = c.to_string();
+            }
+        }
+        println!("{line}");
+        println!("{first_number} {last_number}");
+        let combined_numbers = format!("{first_number}{last_number}");
+        let combined_numbers: u32 = combined_numbers.parse().unwrap();
+        println!("{}", combined_numbers);
+        total += combined_numbers;
+        println!("{}", total);
+    }
+    total
 }
 
 #[cfg(test)]
@@ -14,7 +34,12 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = part1("");
-        assert_eq!(result, "4".to_string());
+        let result: u32 = part1(
+            "1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet",
+        );
+        assert_eq!(result, 142);
     }
 }
